9237e2672f486ae378f8c645eef934f955288f06
Nathalya-Amorim <97710266+Nathalya-Amorim@users.noreply.github.com>
2023-01-18 17:05:29 -0500

    structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+let team = {
+  laura:{
+    name: 'Laura',
+    role: 'Project Manager',
+    avatar: 'laura.jpg'
+  },
+
+  nathalya:{
+    name: 'Nathalya',
+    role: 'Front End',
+    avatar: 'nathalya.jpg'
+  },
+
+  tallyta:{
+    name: 'Tallyta',
+    role: 'Designer',
+    avatar: 'tallyta.jpg'
+  },
+
+  mariia:{
+    name: 'Mariia',
+    role: 'Motion Designer',
+    avatar: 'mariia.jpg'
+  },
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express',
+   content: 'express is easy!'
+  });
 });
+
+router.get('/team/:member', function(req, res) {
+  console.log(req.params.member);
+  res.render('team', { person: team[req.params.member]});
+})
 
 module.exports = router;
